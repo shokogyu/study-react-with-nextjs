f6e6b14ec74ffe9cd231034aa679a02a5fec751c
Shoko Okamoto <mffnoissm23@gmail.com>
2023-09-12 16:31:56 +0900

    ✨ ヘッダーの追加

--- pages/about.js
+++ pages/about.js
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 // import styles from '@/styles/Home.module.css'
 import { Links } from '../components/Links'
 import { Main } from '../components/Main'
+import { Header } from '../components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Header />
       <Main page="about" />
     </>
   )
