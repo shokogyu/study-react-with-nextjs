865e8df5ec7b5e166604e8ed93a137df15896484
Shoko Okamoto <mffnoissm23@gmail.com>
2023-09-12 16:01:25 +0900

    ♻️ 💄 スタイルファイルのリファクタリング＋コンポーネント化

---
+++ components/Headline.js
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import classes from './Headline.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export function Headline(props) {
+  return (
+    <div className={classes.description}>
+      <p>
+        Get started by editing&nbsp;
+        <code className={classes.code}>pages/{props.page}.js</code>
+      </p>
+      <div>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          By{' '}
+          <Image
+            src="/vercel.svg"
+            alt="Vercel Logo"
+            className={classes.vercelLogo}
+            width={100}
+            height={24}
+            priority
+          />
+        </a>
+      </div>
+    </div>
+  )
+}
