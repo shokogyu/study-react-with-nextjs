982f1a706963ac9a585723f453686812e66cda2f
Shoko Okamoto <mffnoissm23@gmail.com>
2023-09-23 17:42:59 +0900

    🚚 Next.jsのファイルシステムルーティングを使って動的にページを生成

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,4 +1,5 @@
 import { usePosts } from "@/src/hooks/usePosts";
+import Link from "next/link";
 
 export const Posts = () => {
   const { data, error, isLoading, isEmpty } = usePosts();
@@ -19,7 +20,11 @@ export const Posts = () => {
     <>
       <ol>
         {data.map((post) => {
-          return <li key={post.id}>{post.title}</li>;
+          return (
+            <li key={post.id}>
+              <Link href={`/post/${post.id}`}>{post.title}</Link>
+            </li>
+          );
         })}
       </ol>
     </>
