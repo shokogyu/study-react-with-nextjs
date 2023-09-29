37c413904466821312e64a0c88ae248988198557
Shoko Okamoto <mffnoissm23@gmail.com>
2023-09-29 19:42:45 +0900

    ⚡️ コメント詳細ページをSG化

--- src/pages/comments/[id]/index.jsx
+++ src/pages/comments/[id]/index.jsx
@@ -1,8 +1,41 @@
 import { Header } from "@/src/components/Header";
 import { Comment as CommentComponent } from "@/src/components/Comment";
 import Head from "next/head";
+import { SWRConfig } from "swr";
 
-const CommentId = () => {
+export const getStaticPaths = async () => {
+  const comments = await fetch("https://jsonplaceholder.typicode.com/comments");
+  const commentsData = await comments.json();
+  const paths = commentsData.map((comment) => {
+    return {
+      params: {
+        id: comment.id.toString(),
+      },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (ctx) => {
+  const { id } = ctx.params;
+  const COMMENT_API_URL = `https://jsonplaceholder.typicode.com/comments/${id}`;
+  const comment = await fetch(COMMENT_API_URL);
+  const commentData = await comment.json();
+
+  return {
+    props: {
+      fallback: {
+        [COMMENT_API_URL]: commentData,
+      },
+    },
+  };
+};
+
+const CommentId = (props) => {
+  const { fallback } = props;
   return (
     <>
       <Head>
@@ -12,7 +45,9 @@ const CommentId = () => {
       <Header />
 
       <main>
-        <CommentComponent />
+        <SWRConfig value={{ fallback }}>
+          <CommentComponent />
+        </SWRConfig>
       </main>
     </>
   );
