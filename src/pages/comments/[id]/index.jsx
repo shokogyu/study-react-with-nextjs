c1e246cb7aebd86cc3e9d49899c2488b3324345e
Shoko Okamoto <mffnoissm23@gmail.com>
2023-09-29 20:47:47 +0900

    ✨ SG化のfallbackをtrueにしてみる

--- src/pages/comments/[id]/index.jsx
+++ src/pages/comments/[id]/index.jsx
@@ -2,9 +2,10 @@ import { Header } from "@/src/components/Header";
 import { Comment as CommentComponent } from "@/src/components/Comment";
 import Head from "next/head";
 import { SWRConfig } from "swr";
+import { useRouter } from "next/router";
 
 export const getStaticPaths = async () => {
-  const comments = await fetch("https://jsonplaceholder.typicode.com/comments");
+  const comments = await fetch("https://jsonplaceholder.typicode.com/comments?_limit=10");
   const commentsData = await comments.json();
   const paths = commentsData.map((comment) => {
     return {
@@ -15,7 +16,7 @@ export const getStaticPaths = async () => {
   });
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 };
 
@@ -35,7 +36,12 @@ export const getStaticProps = async (ctx) => {
 };
 
 const CommentId = (props) => {
+  const router = useRouter();
   const { fallback } = props;
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
   return (
     <>
       <Head>
